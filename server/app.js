dd305f92359723e94d0a2b79337c6973444e8ef9
raissa venegas <raissa.alejandra@gmail.com>
2024-02-28 00:08:03 -0300

    a

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import fileUpload from "express-fileupload"
 import postsRoutes from "./routes/posts.routes.js"
 
 const app = express()
+
 //middleware
 app.use(express.json())
 app.use(fileUpload({
--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ import { MONGODB_URI } from "./config.js";
 
 const connectDB = async() => {
     try{
-        const db= await  mongoose.connect(MONGODB_URI);
+        const db= await mongoose.connect(MONGODB_URI);
         console.log("MongoDB connected to", db.connection.name);
     }catch(error){
         console.log(error);
