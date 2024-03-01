4fdab9437503c1cea9935d115a2755856321a6e9
raissa venegas <raissa.alejandra@gmail.com>
2024-03-01 11:39:22 -0300

    casi

--- server/models/Admin.js
+++ server/models/Admin.js
@@ -5,7 +5,7 @@ const adminSchema = new mongoose.Schema({
     
     nombre: {type:String, required:true, trim:true},
     apellido: {type:String, required:true, trim:true},
-    contraseña: {type:String, required:true, trim:true},
+    contrasena: {type:String, required:true, trim:true},
     correo: {type:String, required:true, trim:true},
     imagen: {url:String, public_id:String},
 })
